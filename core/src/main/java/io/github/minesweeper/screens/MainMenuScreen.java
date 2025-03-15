60ebdb5cc4948397fb472e353c1772e71ddb5704
Samuel Ogg <56794292+SamO135@users.noreply.github.com>
2025-03-15 01:33:15 +0000

    add difficulty select to main menu

--- core/src/main/java/io/github/minesweeper/screens/MainMenuScreen.java
+++ core/src/main/java/io/github/minesweeper/screens/MainMenuScreen.java
@@ -2,12 +2,12 @@ package io.github.minesweeper.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import io.github.minesweeper.game.MinesweeperGame;
+import io.github.minesweeper.settings.Difficulty;
 import io.github.minesweeper.ui.CustomButton;
 
 public class MainMenuScreen extends MenuScreen {
@@ -18,21 +18,40 @@ public class MainMenuScreen extends MenuScreen {
         Texture titleText = new Texture(Gdx.files.internal("text/minesweeper_text.png"));
         Image titleTextImage = new Image(new TextureRegionDrawable(new TextureRegion(titleText)));
 
-        TextureAtlas buttonAtlas = new TextureAtlas(Gdx.files.internal("buttons/start_button/start_button.atlas"));
-        CustomButton startButton = new CustomButton(buttonAtlas, this::onStartClick);
+        // create buttons
+        CustomButton easyButton = createCustomButton("buttons/easy/easy_text.atlas", this::onEasyClick);
+        CustomButton normalButton = createCustomButton("buttons/normal/normal_text.atlas", this::onNormalClick);
+        CustomButton hardButton = createCustomButton("buttons/hard/hard_text.atlas", this::onHardClick);
 
         Table table = new Table();
         table.setFillParent(true);
         table.add(titleTextImage).width(uiHeight * .9f).height(uiHeight * .082f);
         table.getCell(titleTextImage).expand().top().padTop(uiHeight * .1f);
         table.row();
-        table.add(startButton).width(uiWidth * .3f).height(uiHeight * .06f);
-        table.getCell(startButton).expand().bottom().padBottom(uiHeight * .25f);
+        table.add(easyButton).width(uiWidth * .2f).height(uiHeight * .05f);
+        table.getCell(easyButton).padBottom(uiHeight * .05f);
+        table.row();
+        table.add(normalButton).width(uiWidth * .3f).height(uiHeight * .05f);
+        table.getCell(normalButton).padBottom(uiHeight * .05f);
+        table.row();
+        table.add(hardButton).width(uiWidth * .2f).height(uiHeight * .05f);
+        table.getCell(hardButton).padBottom(uiHeight * .2f);
 
         stage.addActor(table);
     }
 
-    private void onStartClick() {
+    private void onEasyClick() {
+        game.difficulty = Difficulty.EASY;
+        game.setScreen(new GameScreen(game));
+    }
+
+    private void onNormalClick() {
+        game.difficulty = Difficulty.MEDIUM;
+        game.setScreen(new GameScreen(game));
+    }
+
+    private void onHardClick() {
+        game.difficulty = Difficulty.HARD;
         game.setScreen(new GameScreen(game));
     }
 }
--- core/src/main/java/io/github/minesweeper/screens/MenuScreen.java
+++ core/src/main/java/io/github/minesweeper/screens/MenuScreen.java
@@ -4,10 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import io.github.minesweeper.game.MinesweeperGame;
+import io.github.minesweeper.ui.ButtonClickHandler;
+import io.github.minesweeper.ui.CustomButton;
 
 abstract class MenuScreen implements Screen {
     protected final MinesweeperGame game;
@@ -74,6 +77,11 @@ abstract class MenuScreen implements Screen {
     public void dispose() {
         spriteBatch.dispose();
         stage.dispose();
+    }
+
+    public CustomButton createCustomButton(String atlasPath, ButtonClickHandler buttonClickHandler) {
+        TextureAtlas buttonAtlas = new TextureAtlas(Gdx.files.internal(atlasPath));
+        return new CustomButton(buttonAtlas, buttonClickHandler);
     }
 
 }
